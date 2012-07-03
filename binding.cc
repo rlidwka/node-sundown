860686d5d3ada4ec70e5aeee16d664b7ffbf6156
Alex Kocharin <alex@kocharin.ru>
2012-07-03 06:29:13 +0400

    markdown extensions

--- binding.cc
+++ binding.cc
@@ -3,8 +3,8 @@
 #include <string.h>
 
 extern "C" {
-#	include "redcarpet/html.h"
-#	include "redcarpet/markdown.h"
+#	include "sundown/html.h"
+#	include "sundown/markdown.h"
 }
 
 using namespace v8;
@@ -25,43 +25,92 @@ Handle<Value> VException(const char *msg)
 	return ThrowException(Exception::Error(String::New(msg)));
 }
 
+void get_md_flags(Handle<Object> hash, unsigned int *enabled_extensions_p)
+{
+	unsigned int extensions = 0;
+
+	/**
+	 * Markdown extensions -- all disabled by default
+	 */
+
+	if (hash->Get(String::NewSymbol("no_intra_emphasis"))->BooleanValue())
+		extensions |= MKDEXT_NO_INTRA_EMPHASIS;
+
+	if (hash->Get(String::NewSymbol("tables"))->BooleanValue())
+		extensions |= MKDEXT_TABLES;
+
+	if (hash->Get(String::NewSymbol("fenced_code_blocks"))->BooleanValue())
+		extensions |= MKDEXT_FENCED_CODE;
+
+	if (hash->Get(String::NewSymbol("autolink"))->BooleanValue())
+		extensions |= MKDEXT_AUTOLINK;
+
+	if (hash->Get(String::NewSymbol("strikethrough"))->BooleanValue())
+		extensions |= MKDEXT_STRIKETHROUGH;
+
+	if (hash->Get(String::NewSymbol("lax_html_blocks"))->BooleanValue())
+		extensions |= MKDEXT_LAX_HTML_BLOCKS;
+
+	if (hash->Get(String::NewSymbol("space_after_headers"))->BooleanValue())
+		extensions |= MKDEXT_SPACE_HEADERS;
+
+	if (hash->Get(String::NewSymbol("superscript"))->BooleanValue())
+		extensions |= MKDEXT_SUPERSCRIPT;
+
+	*enabled_extensions_p = extensions;
+}
+
 Handle<Value> markdown_render(const Arguments &args)
 {
 	HandleScope scope;
+	
 	unsigned int extensions = 0;
-
 	struct rb_redcarpet_rndr rndr;
 	struct sd_markdown *markdown;
+
+	// parse arguments
+	int arg_len = args.Length();
+	if (arg_len < 1 || arg_len > 2) {
+		return VException("Usage: Sundown.render(text, extensions={})");
+	}
+
+	// get md flags
+	Local<String> text_to_render = args[0]->ToString();
+	if (arg_len == 2) {
+		if (!args[1]->IsObject()) {
+			return VException("Markdown: 2nd argument isn't an object");
+		}
+		Local<Object> hash = Object::Cast(*args[1]);
+		get_md_flags(hash, &extensions);
+	}
+
+	// initialize renderer TODO
 	sdhtml_renderer(&rndr.callbacks, (struct html_renderopt *)&rndr.options.html, 0);
 
-//	markdown = sd_markdown_new(extensions, 16, &rndr->callbacks, &rndr->options);
-//	if (!markdown)
-//		rb_raise(rb_eRuntimeError, "Failed to create new Renderer class");
 	markdown = sd_markdown_new(extensions, 16, &rndr.callbacks, &rndr.options);
 	if (!markdown)
 		return VException("Failed to create new Renderer class");
 
-//	rb_markdown = Data_Wrap_Struct(klass, NULL, rb_redcarpet_md__free, markdown);
-//	rb_iv_set(rb_markdown, "@renderer", rb_rndr);
-// TODO
+	// now rendering:
 	
 	struct buf *output_buf;
 	/* initialize buffers */
-	output_buf = bufnew(128);
+	output_buf = bufnew(1024);
 
-	Local<String> tt = args[0]->ToString();
-	char text[65536];
-	tt->WriteUtf8(text, -1, 0, 0);
+	int size = text_to_render->Utf8Length();
+	char* text = (char*)malloc(size+1);
+	text_to_render->WriteUtf8(text, size);
 	/* render the magic */
 	sd_markdown_render(
 		output_buf,
 		(const unsigned char*)text,
-		strlen(text),
+		size,
 		markdown);
 
 	Local<String> result = String::New((const char*)output_buf->data, output_buf->size);
 
 	bufrelease(output_buf);
+	free(text);
 
 	return scope.Close(result);
 }
